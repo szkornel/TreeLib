a770ba57c773d00d01ef3e250a90357da4fef6a5
Szabó Kornél <szkornel005@gmail.com>
2023-06-03 16:00:17 +0200

    Add tests

--- TreeLib.Test/Program.cs
+++ TreeLib.Test/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TreeLib.Test
 {
@@ -10,6 +6,21 @@ namespace TreeLib.Test
     {
         static void Main(string[] args)
         {
+            BST<int> BST = new BST<int>();
+
+            foreach (int element in new int[7] { 18, 20, 30, 40, 48, 50, 53 })
+            {
+                BST.Insert(element);
+            }
+
+            Console.WriteLine($"Bejárás sorrendben ({BST.Count} elem):");
+            BST.TraverseInOrder();
+
+            Console.WriteLine($"\nA fa {BST.Depth} szintből áll kiegyenlítés előtt.");
+            BST.Balance();
+            Console.WriteLine($"A fa {BST.Depth} szintből áll kiegyenlítés után.");
+
+            Console.ReadKey();
         }
     }
 }
